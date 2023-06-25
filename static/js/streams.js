7bced3d1146d76764a2b1498608e8bc5df962289
talhabinalam <talhabinalam98@gmail.com>
2023-06-26 00:50:17 +0600

    updated model

--- static/js/streams.js
+++ static/js/streams.js
@@ -3,6 +3,8 @@ const CHANNEL = sessionStorage.getItem('room')
 const TOKEN = sessionStorage.getItem('token')
 let UID = Number(sessionStorage.getItem('UID'))
 
+let NAME = sessionStorage.getItem('name')
+
 const client = AgoraRTC.createClient({mode:'rtc', codec:'vp8'})
 
 let localTracks = []
